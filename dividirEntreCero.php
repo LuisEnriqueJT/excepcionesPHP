6ec89c66e70f0b809e55c46faf3d53a674267b3d
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-01-07 01:10:33 -0600

    Se agrega validacion para saber si se recibe el GET

--- dividirEntreCero.php
+++ dividirEntreCero.php
@@ -1,11 +1,24 @@
 <?php
 	try {
 		$variable = 100;
-		if($_GET['divisor']!=0){
-			echo "El resultado de la division es: ".$variable/$_GET['divisor'];
+		
+		//Se valida si existe el divisor
+		if(isset($_GET['divisor'])){
+			
+			//inicio de la validacion del divisor
+			if($_GET['divisor']!=0){
+				echo "El resultado de la division es: ".$variable/$_GET['divisor'];
+			}else{
+				//Entra aquí si el divisor es 0
+				throw new Exception("No se puede dividir entre 0<br>");
+			}
+
 		}else{
-			throw new Exception("No se puede dividir entre 0<br>");
-		}		
+			//Entra aquí si no se pasa el parametro por el GET
+			throw new Exception("No existe el divisor");
+			
+		}
+
 		
 	} catch (Exception $e) {
 		echo "<h1>Existe un erro en tu operacion: ".$e->getMessage()."</h1><br/>";
